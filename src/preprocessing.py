8f5a1e63a705fa0d8e75d83845ff461186d48077
Pallavi Krishnamoorthi <krishnamoorthipallavi@gmail.com>
2026-06-07 12:29:46 +0530

    preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,2 +1,21 @@
+import pandas as pd
 
-# to be implemented
+def load_data(filepath):
+    return pd.read_csv(filepath)
+
+def preprocess(df):
+
+    categorical_cols = [
+        "age_group",
+        "app_category",
+        "emotion",
+        "time_of_day"
+    ]
+
+    df = pd.get_dummies(
+        df,
+        columns=categorical_cols,
+        drop_first=True
+    )
+
+    return df
