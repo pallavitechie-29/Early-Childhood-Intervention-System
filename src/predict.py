cb7c0adfb37ad39f317c06423f6659b4c388d423
Pallavi Krishnamoorthi <krishnamoorthipallavi@gmail.com>
2026-06-07 12:30:37 +0530

    predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,2 +1,11 @@
-# to be implemented
+import joblib
 
+model = joblib.load(
+    "../models/intervention_model.pkl"
+)
+
+def predict_intervention(data):
+
+    result = model.predict(data)
+
+    return result
